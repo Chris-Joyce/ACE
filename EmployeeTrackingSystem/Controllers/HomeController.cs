6b42ac11e90247da422a6845c8b66d84a85bdf20
Chris <christopher1995joyce@gmail.com>
2019-03-06 21:28:46 -0500

    Renamed Reports excel file

--- EmployeeTrackingSystem/Controllers/HomeController.cs
+++ EmployeeTrackingSystem/Controllers/HomeController.cs
@@ -97,7 +97,7 @@ namespace EmployeeTrackingSystem.Controllers
             return File(
                 fileContents: fileContents,
                 contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                fileDownloadName: "Report.xlsx"
+                fileDownloadName: DateTime.Now.ToString("MM_dd_yy") + "_Report.xlsx"
             );
         }
 
